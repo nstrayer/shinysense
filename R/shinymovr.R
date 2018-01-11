ebee58250bf4b5a82c9518587b25d3d93a4e93eb
Nick Strayer <nick.strayer@gmail.com>
2018-01-11 13:06:39 -0600

    added more css customization options to the button

--- R/shinymovr.R
+++ R/shinymovr.R
@@ -1,12 +1,22 @@
 #' Use shiny on your mobile phone to gather accelorameter data for whatever your heart could desire.
 #' @param id the id you will use to keep track of this component in your app
+#' @param resting_msg the string displayed when the button is not pressed. Defaults to "Turn On"
+#' @param button_width width of the button in a valid css string. E.g. needs to have pixels appended to it.
+#' @param button_height height of the button, again in valid css string.
 #' @param button_color valid css string to control the button color.
 #' @return A blue button that you press to initiate or stop recording of acceloration data.
 #' @export
 #' @examples
-#' shinymovrUI('movrButton')
-shinymovrUI <- function(id, button_color = 'steelblue') {
+#' shinymovrUI('movrButton', resting_msg = 'Click me to record', button_width = '200px')
+shinymovrUI <- function(id,
+                        resting_msg = 'Turn On',
+                        button_width = '130px',
+                        button_height = '50px',
+                        button_color = 'steelblue'
+                        ) {
   ns <- NS(id)
+
+  # convert the css
 
   # Grab the external javascript and css
   gyronorm <- .get_script("libraries/gyronorm.js", "js")
@@ -25,8 +35,11 @@ shinymovrUI <- function(id, button_color = 'steelblue') {
     tags$button(
       id = ns("movr"),
       class = 'movr-button',
-      style = sprintf("background-color: %s", button_color),
-      "Turn On"
+      style = sprintf(
+        "background-color: %s; width: %s; height:%s",
+        button_color, button_width, button_height
+      ),
+      resting_msg
     )
   ) # end tag list.
 }
