333d4f8c0b066c00753e14cbe518d8d444cd7c13
Nick Strayer <nick.strayer@gmail.com>
2018-07-18 17:51:31 +0000

    Placeholder R script with link to javascript tutorial for webcam access.

---
+++ R/shinyviewr.R
@@ -0,0 +1,3 @@
+# This is a good tutorial for getting webcam access https://developer.mozilla.org/en-US/docs/Web/API/WebRTC_API/Taking_still_photos
+
+# The main question will be should I do this in htmlwidgets or r2d3?
