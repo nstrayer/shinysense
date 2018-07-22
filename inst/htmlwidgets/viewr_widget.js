b4afbfff59087f21ad5459d22bc6d01da7ee109a
Nick Strayer <nick.strayer@gmail.com>
2018-07-22 23:27:48 +0000

    Added support for rear cameras! A smart popup will show up if a rear camera is detected allowing the user to switch.

--- inst/htmlwidgets/viewr_widget.js
+++ inst/htmlwidgets/viewr_widget.js
@@ -8,7 +8,10 @@ HTMLWidgets.widget({
 
     const container = d3.select(el).html('');
     const shutter_height = 40;
-    const shutter = container.append('center').append('button')
+
+    const shutterHolder = container.append('center');
+
+    const shutter = shutterHolder.append('button')
       .text('Take Photo!')
       .style('width', '50%')
       .style('max-width', '300px')
@@ -36,40 +39,90 @@ HTMLWidgets.widget({
 
     const ctx = canvas.node().getContext('2d');
 
+    let videoStream;
+
     return {
 
       renderValue: function(x) {
 
         const outputWidth = x.outputWidth === null ? width: x.outputWidth;
         const outputHeight = x.outputHeight === null ? height: x.outputHeight;
+        function startStream(stream) {
+          video.src = window.URL.createObjectURL(stream);
+          video.play();
+          shutter.on('click', () => {
+            // draw current video frame to the invisible canvas element
+            ctx.drawImage(video, 0, 0, outputWidth, outputHeight);
+
+            // send the current canvas state to shiny as a base64 encoded string
+            Shiny.onInputChange(
+              el.id + "_photo",
+              canvas.node().toDataURL("image/png")
+            );
+           }); // end on('click')
+        }
+        // kills all currently running video streams
+        function stopMediaTracks(stream) {
+          stream.getTracks().forEach(track => {
+            track.stop();
+          });
+        }
+
+        function getCameraImage(facingMode = 'user'){
+          if (typeof videoStream !== 'undefined') {
+            stopMediaTracks(videoStream);
+          }
+
+          navigator.mediaDevices
+            .getUserMedia({
+              video: {
+                facingMode,
+                width: outputWidth,
+                height: outputHeight
+              }
+            })
+            .then(startStream);
+        }
 
         canvas
           .attr('width', outputWidth)
           .attr('height', outputHeight);
 
         if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+
           navigator.mediaDevices
-            .getUserMedia({
-              video: {
-                width: outputWidth,
-                height: outputHeight
-              }
-            })
-            .then(function(stream) {
-               video.src = window.URL.createObjectURL(stream);
-               video.play();
-               shutter.on('click', () => {
+            .enumerateDevices()
+            .then( devices => {
 
-                 // draw current video frame to the invisible canvas element
-                 ctx.drawImage(video, 0, 0, outputWidth, outputHeight);
+              const numberVideoDevices = devices
+                .filter(d => d.kind === 'videoinput')
+                .length;
 
-                 // send the current canvas state to shiny as a base64 encoded string.
-                 Shiny.onInputChange(
-                   el.id + "_photo",
-                   canvas.node().toDataURL("image/png")
-                 );
-               }); // end on('click')
-            }); // end .then
+              if(numberVideoDevices > 1){
+                const cameraChooser = shutterHolder
+                  .append('select')
+                  .style('height', `${shutter_height}px`)
+                  .style('font-size', '24px')
+                  .style('font-family', 'Optima');
+
+                cameraChooser.selectAll('option').data(
+                    [
+                      {value: 'user', title: 'front'},
+                      {value:'environment', title: 'rear'}
+                    ]
+                  )
+                  .enter().append('option')
+                  .attr('value', d => d.value)
+                  .text(d => d.title);
+
+                cameraChooser.on('change', function(){
+                    const selectValue = d3.select(this).property('value');
+                    getCameraImage(selectValue);
+                  });
+                }
+            });
+
+            getCameraImage();
           } // end if
       },
        resize: function(width, height) {
