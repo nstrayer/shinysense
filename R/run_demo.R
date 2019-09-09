94d73b29b33b2517dcba36f6ff18c83d169f7588
Nick Strayer <nick.strayer@gmail.com>
2019-09-09 14:01:19 -0500

    shuffled around demos to be the name of their function and have code showed when they are run by default.

--- R/run_demo.R
+++ R/run_demo.R
@@ -2,18 +2,21 @@
 #' Load and play with simple demo apps using the different senses available in shinysense. Currently supports
 #' `draw_distribution` which allows the user to draw a beta distribution and get its parameters.`swipr` which demos basic
 #' usage of cards and decision recording with `shinyswipr`.
-#' @param demo_name Name of demo app to run. Options `{'draw_distribution', 'swipr'}`.
+#' @param demo_name Name of demo app to run. Options `{'shinydrawr', 'shinyviewr', 'shinyswipr'}`.
+#' @param show_code See the underlying code for the demo app? Defaults to `TRUE`./
 #'
 #' @return Shiny app instance
 #' @export
 #'
 #' @examples
-#' run_demo('draw_distribution')
-run_demo <- function(demo_name){
+#' run_demo('shinydrawr')
+run_demo <- function(demo_name, show_code = TRUE){
 
   # Grab app script location
-  app_script <- system.file(paste0('demo_apps/', demo_name, '.R'), package = 'shinysense')
+  app_script <- system.file(paste0('demo_apps/', demo_name, '/app.R'), package = 'shinysense')
 
   # Run app
-  shiny::shinyAppFile(app_script)
+  run_options <- if (show_code) c('display.mode' = 'showcase') else c()
+
+  shiny::shinyAppFile(app_script, options = run_options)
 }
