0f23062d295c840426ae512367daacdc13a41dfa
Nick Strayer <nick.strayer@gmail.com>
2019-09-11 10:24:06 -0500

    Updated the shinyviewr docs to be actually properly formatted.

--- R/shinyviewr.R
+++ R/shinyviewr.R
@@ -1,15 +1,24 @@
-#' A webcam view and snapshot function. Will send a 3d array (width, height, colors+opacity) back to your computer of the frame when you click take photo.
+#' Access data from user's cameras: UI function
+#'
+#' A webcam view and snapshot function. Will send a 3d array (width, height,
+#' colors+opacity) back to your computer of the frame when you click take photo.
+#' Paired with the server function \code{\link{shinyviewr}}.
+#'
+#' @seealso \code{\link{shinyviewr}}
 #' @param id the id you will use to keep track of this component in your app
-#' @param height Must be a valid CSS unit (like \code{'100\%'},
-#'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
-#'   string and have \code{'px'} appended.
+#' @param height Must be a valid CSS unit (like \code{'100\%'}, \code{'400px'},
+#'   \code{'auto'}) or a number, which will be coerced to a string and have
+#'   \code{'px'} appended.
+#'
 #' @return A video overlay and a 'shutter' button.
-#' @export
+#'
 #' @examples
 #' \dontrun{
 #' shinyviewrUI('myrecorder')
 #' }
+#'
 #' @import shiny
+#' @export
 shinyviewr_UI <- function(id, height = '400px'){
   # Create a namespace function using the provided id
   ns <- NS(id)
@@ -19,24 +28,36 @@ shinyviewr_UI <- function(id, height = '400px'){
 }
 
 
-#' Gather recorded data from UI.
+#' Access data from user's cameras: Server function
 #'
-#' Upon completion of line draw, returns a reactive variable that contains a vector of the y coordinates of what the user has drawn. This also includes the start point specified with `draw_start`
-#'     This is the server component of shinyviewr. You never directly use this function but instead call it through the shiny function `callModule()`. See the example for how to do this.
+#' Upon completion of line draw, returns a reactive variable that contains a
+#' vector of the y coordinates of what the user has drawn. This also includes
+#' the start point specified with `draw_start` This is the server component of
+#' shinyviewr. You never directly use this function but instead call it through
+#' the shiny function `callModule()`. See the example for how to do this. Paired
+#' with the UI function \code{\link{shinyviewr_UI}}.
 #'
+#' @seealso \code{\link{shinyviewr_UI}}
 #' @param input you can ignore this as it is taken care of by shiny
 #' @param output you can ignore this as it is taken care of by shiny
 #' @param session you can ignore this as it is taken care of by shiny
-#' @param output_width, output_height How many pixels wide or tall you want your returned photos/the view of the webcam.
-#'   When left unfilled or set to \code{NULL} this will attempt to fill whatever size your UI element is. For many image
-#'   related tasks you want the output to be a square. So setting this to something like 300x300 is a good idea.
-#' @return A reactive function that will return a 3D array with dimensions \code{(height, width, channels (RGBA))} corresponding to the
-#'   image taken by the webcam when shutter was pressed. The RGBA are all in the range of 0-1.
-#' @export
+#' @param output_width,output_height How many pixels wide or tall you want your
+#'   returned photos/the view of the webcam. When left unfilled or set to
+#'   \code{NULL} this will attempt to fill whatever size your UI element is. For
+#'   many image related tasks you want the output to be a square. So setting
+#'   this to something like 300x300 is a good idea.
+#'
+#'
+#' @return A reactive function that will return a 3D array with dimensions
+#'   \code{(height, width, channels (RGBA))} corresponding to the image taken by
+#'   the webcam when shutter was pressed. The RGBA are all in the range of 0-1.
+#'
 #' @examples
 #' \dontrun{
 #' camera_snapshot <- callModule( shinyviewr, 'my_camera' )
 #' }
+#'
+#' @export
 shinyviewr <- function(
   input, output, session,
   output_width = 300,
